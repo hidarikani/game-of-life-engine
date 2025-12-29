09da27ecb5720a7c5bec33366b385c0a198f40ba
hidarikani <19387605+hidarikani@users.noreply.github.com>
2025-12-29 22:20:52 +0200

    Refined constant declarations

--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,15 @@
-export enum GridMode {
-  Finite,
-  Toroidal,
-}
+export const MIN_WORLD_WIDTH = 3 as const;
+export const MIN_WORLD_HEIGHT = 3 as const;
 
-export const MIN_WORLD_WIDTH = 3;
-export const MIN_WORLD_HEIGHT = 3;
-
-export const CELL_ALIVE_CHAR = "#";
-export const CELL_DEAD_CHAR = ".";
+export const CELL_ALIVE_CHAR = "#" as const;
+export const CELL_DEAD_CHAR = "." as const;
 
 export const CELL_CHAR_TO_BOOL: Record<string, boolean> = {
   [CELL_ALIVE_CHAR]: true,
   [CELL_DEAD_CHAR]: false,
 };
+
+export const GRID_MODES = {
+  FINITE: "Finite",
+  TOROIDAL: "Toroidal",
+} as const;
