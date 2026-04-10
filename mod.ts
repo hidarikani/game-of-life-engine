1e9b68fb3853d713e745aa158c3cee93889b02ff
hidarikani <19387605+hidarikani@users.noreply.github.com>
2026-04-10 21:08:27 +0300

    New version

--- mod.ts
+++ mod.ts
@@ -1,6 +1,13 @@
+export { Grid } from "./src/grid/grid.ts"
 export { Engine } from "./src/engine/engine.ts";
 export type {
-  EngineOptions as WorldOptions,
+  EngineOptions,
   GridMode,
+  GridOptionsFromLiveCells,
+  GridOptionsFromString,
   GridSize,
+  IEngine,
+  IGrid,
+  PlacementMode,
+  Point,
 } from "./src/types.ts";
