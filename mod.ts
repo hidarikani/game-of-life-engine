efda393142225f68b093bb8b181e5e18f30bc402
hidarikani <19387605+hidarikani@users.noreply.github.com>
2026-09-03 17:16:44 +0300

    export * types

--- mod.ts
+++ mod.ts
@@ -1,29 +1,17 @@
 // Types
-export type { Point } from "./src/types/geometry.ts";
-export type { CellChars, CellKey, LiveCells } from "./src/types/cell.ts";
-export type {
-  GridMode,
-  GridOptionsFromLiveCells,
-  GridOptionsFromString,
-  GridSize,
-  IGrid,
-  PlacementMode,
-} from "./src/types/grid.ts";
-export type { EngineOptions, IEngine } from "./src/types/engine.ts";
-export type {
-  IPatternLib,
-  Pattern,
-  PatternFilter,
-  PatternType,
-} from "./src/types/pattern.ts";
+export type * from "./src/types/geometry.ts";
+export type * from "./src/types/cell.ts";
+export type * from "./src/types/grid.ts";
+export type * from "./src/types/engine.ts";
+export type * from "./src/types/pattern.ts";
 
 // Classes
-export { Grid } from "./src/grid/grid.ts";
-export { Engine } from "./src/engine/engine.ts";
-export { PatternLib } from "./src/patterns/pattern.ts";
+export * from "./src/grid/grid.ts";
+export * from "./src/engine/engine.ts";
+export * from "./src/patterns/pattern.ts";
 
 // Constants
-export { GRID_MODES, PATTERN_TYPES, PLACEMENT_MODES } from "./src/constants.ts";
+export * from "./src/constants.ts";
 
 // Utilities
 export { pointToCellKey } from "./src/seed/seed.ts";
